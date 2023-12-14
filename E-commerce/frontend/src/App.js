0c606a099c4a5cfcd5a5bb4524d0ee495dad1f94
pallavim34 <120103352+pallavim34@users.noreply.github.com>
2023-12-14 14:56:54 +0530

    Add files via upload

---
+++ E-commerce/frontend/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import LoginSignup from './Pages/LoginSignup';
+
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        
+          <Route path='/login' element={<LoginSignup/>}/>
+         
+          
+      </Routes>
+      </BrowserRouter>
+    
+      
+    </div>
+  );
+};
+
+export default App;
